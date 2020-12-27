c7bbf9c3f617bf5c37ce8b45ef4c99076051cc96
Hamad Al Marri <hamad.s.almarri@gmail.com>
2020-12-27 16:26:20 +0300

    fix: reset prev flag in finish_context_switch

--- cachy debug helper files/runsys.sh
+++ cachy debug helper files/runsys.sh
@@ -1,4 +1,6 @@
-qemu-system-x86_64 -kernel arch/x86/boot/bzImage \
+#! /bin/bash
+
+clear && qemu-system-x86_64 -kernel arch/x86/boot/bzImage \
 -boot c -m 2049M -hda ../buildroot/output/images/rootfs.ext4 \
 -append "root=/dev/sda rw console=ttyS0,115200 acpi=off nokaslr" \
 -serial stdio -display none -smp $1
